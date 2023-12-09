7cdfe830a775310d9959d4035718f37b3b33ac93
jhoen13 <sahileth96@gmail.com>
2023-12-09 17:15:42 -0500

    se agrego la alerta al codigo CSV

--- public/views/models/admin/archivo_csv/recibe_excel.php
+++ public/views/models/admin/archivo_csv/recibe_excel.php
@@ -2,68 +2,72 @@
 
 <?php
 require('config.php');
-$tipo       = $_FILES['produc']['type'];
-$tamanio    = $_FILES['produc']['size'];
-$archivotmp = $_FILES['produc']['tmp_name'];
-$lineas     = file($archivotmp);
 
-$i = 0;
+// Verificar si se ha subido un archivo
+if ($_FILES['produc']['error'] !== UPLOAD_ERR_OK || empty($_FILES['produc']['tmp_name']) || $_FILES['produc']['size'] === 0) {
+    // Mostrar una alerta si no se seleccionó ningún archivo
+    // echo '<script>alert("Por favor, selecciona un archivo para subir.");</script>';
+    echo '<script>alert("Por favor, selecciona un archivo para subir."); window.location.href = "./index.php";</script>';
+} else {
+    // Procesar el archivo si se seleccionó correctamente
+    $tipo       = $_FILES['produc']['type'];
+    $tamanio    = $_FILES['produc']['size'];
+    $archivotmp = $_FILES['produc']['tmp_name'];
+    $lineas     = file($archivotmp);
 
-foreach ($lineas as $linea) {
-    $cantidad_registros = count($lineas);
-    $cantidad_regist_agregados =  ($cantidad_registros - 1);
+    $i = 0;
+    foreach ($lineas as $linea) {
+        $cantidad_registros = count($lineas);
+        $cantidad_regist_agregados = ($cantidad_registros - 1);
 
+        if ($i != 0) {
+            $datos = str_getcsv($linea, ',', '"');
 
+            // Asignación de valores
+            $nombre = !empty($datos[0]) ? ($datos[0]) : '';
+            $descrip = !empty($datos[1]) ? ($datos[1]) : '';
+            $prec_com = !empty($datos[2]) ? ($datos[2]) : '';
+            $dispo = !empty($datos[3]) ? ($datos[3]) : '';
+            $cate = !empty($datos[4]) ? ($datos[4]) : '';
+            $canti = !empty($datos[5]) ? ($datos[5]) : '';
+            $embala = !empty($datos[6]) ? ($datos[6]) : '';
+            $foto = !empty($datos[7]) ? ($datos[7]) : '';
+            $prec_ven = !empty($datos[8]) ? ($datos[8]) : '';
+            $docu = !empty($datos[9]) ? ($datos[9]) : '';
 
-    if ($i != 0) {
-        // $datos = preg_split('/[,;"\s]+/', $linea, -1, PREG_SPLIT_NO_EMPTY);
-        // print_r($datos);
+            // Consulta de inserción
+            $insertar = "INSERT INTO productos( 
+                nom_produc,
+                descrip,
+                precio_compra,
+                disponibles,
+                id_categoria,
+                cantidad,
+                id_embala,
+                foto,
+                precio_ven,
+                documento
+            ) VALUES(
+                '$nombre',
+                '$descrip',
+                '$prec_com',
+                '$dispo',
+                '$cate',
+                '$canti',
+                '$embala',
+                '$foto',
+                '$prec_ven',
+                '$docu'
+            )";
 
-        // $datos = explode(",", $linea);
-        $datos = str_getcsv($linea, ',', '"');
+            // Ejecutar consulta
+            mysqli_query($con, $insertar);
+        }
 
-        $nombre = !empty($datos[0])  ? ($datos[0]) : '';
-        $descrip = !empty($datos[1])  ? ($datos[1]) : '';
-        $prec_com = !empty($datos[2])  ? ($datos[2]) : '';
-        $dispo = !empty($datos[3])  ? ($datos[3]) : '';
-        $cate = !empty($datos[4])  ? ($datos[4]) : '';
-        $canti = !empty($datos[5])  ? ($datos[5]) : '';
-        $embala = !empty($datos[6])  ? ($datos[6]) : '';
-        $foto = !empty($datos[7])  ? ($datos[7]) : '';
-        $prec_ven = !empty($datos[8])  ? ($datos[8]) : '';
-        $docu = !empty($datos[9])  ? ($datos[9]) : '';
-
-        $insertar = "INSERT INTO productos( 
-            nom_produc,
-            descrip,
-            precio_compra,
-            disponibles,
-            id_categoria,
-            cantidad,
-            id_embala,
-            foto,
-            precio_ven,
-            documento
-        ) VALUES(
-            '$nombre',
-            '$descrip',
-            '$prec_com',
-            '$dispo',
-            '$cate',
-            '$canti',
-            '$embala',
-            '$foto',
-            '$prec_ven',
-            '$docu'
-        )";
-        mysqli_query($con, $insertar);
+        echo '<div>' . $i . "). " . $linea . '</div>';
+        $i++;
     }
-
-    echo '<div>' . $i . "). " . $linea . '</div>';
-    $i++;
+    // total de registros realizados
+    echo '<p style="text-aling:center; color:#333;">Total de Registros: ' . $cantidad_regist_agregados . '</p>';
 }
-
-
-echo '<p style="text-aling:center; color:#333;">Total de Registros: ' . $cantidad_regist_agregados . '</p>';
-
 ?>
