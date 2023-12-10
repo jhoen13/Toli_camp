b230bf740f855f2c61709d692cba70a8638e22c1
jhoen13 <sahileth96@gmail.com>
2023-12-10 16:32:49 -0500

    modificaciones en el diseño de auth/resgistro.php

--- public/views/auth/registro.php
+++ public/views/auth/registro.php
@@ -39,17 +39,17 @@ if ((isset($_POST["btn-registrar"]))) {
   $estado = $_POST['id_estado'];
 
   if (!empty($_FILES['foto']['name'])) {
-      $extension = pathinfo($_FILES['foto']['name'], PATHINFO_EXTENSION);
-      $nom = "usuario_" . time();
-      $foto_nombre = $nom . "." . $extension;
-      $ruta_destino = "../../assets/img/img_user/$foto_nombre";
+    $extension = pathinfo($_FILES['foto']['name'], PATHINFO_EXTENSION);
+    $nom = "usuario_" . time();
+    $foto_nombre = $nom . "." . $extension;
+    $ruta_destino = "../../assets/img/img_user/$foto_nombre";
 
-      // Mueve el archivo a la ruta de destino
-      move_uploaded_file($_FILES['foto']['tmp_name'], $ruta_destino);
+    // Mueve el archivo a la ruta de destino
+    move_uploaded_file($_FILES['foto']['tmp_name'], $ruta_destino);
   } else {
-      // Si no se envió un archivo, muestra un mensaje de error y redirige
-      echo '<script>alert("No se ha seleccionado una imagen"); window.location="./index.php"</script>';
-      exit(); // Detiene la ejecución del script
+    // Si no se envió un archivo, muestra un mensaje de error y redirige
+    echo '<script>alert("No se ha seleccionado una imagen"); window.location="./index.php"</script>';
+    exit(); // Detiene la ejecución del script
   }
 
   $consulta2 = $con->prepare("SELECT * FROM usuarios WHERE documento= '$documento' OR correo_electronico = '$email'");
@@ -57,35 +57,35 @@ if ((isset($_POST["btn-registrar"]))) {
   $consull = $consulta2->fetchAll();
 
   if ($consull) {
-      // SI SE CUMPLE ESTA CONSULTA ES PORQUE EL DOCUMENTO O EL EMAIL YA EXISTEN EN LA DB
-      echo '<script> alert ("// Estimado Usuario, los datos ingresados ya están registrados. //");</script>';
-      echo '<script>windows.location="registro.php"</script>';
+    // SI SE CUMPLE ESTA CONSULTA ES PORQUE EL DOCUMENTO O EL EMAIL YA EXISTEN EN LA DB
+    echo '<script> alert ("// Estimado Usuario, los datos ingresados ya están registrados. //");</script>';
+    echo '<script>windows.location="registro.php"</script>';
   } elseif ($documento == "" || $nombre == "" || $apellido == "" || $email == "" || $pass == "" || $celular == "" || $direc == "" || $genero == "" || $rol == "" || $foto_nombre == "" || $estado == "") {
-      // CONDICIONAL DEPENDIENDO SI EXISTEN DATOS VACÍOS EN EL FORMULARIO 
-      echo '<script> alert ("Estimado Usuario, Existen Datos Vacios En El Formulario");</script>';
-      echo '<script>windows.location="registro.php"</script>';
+    // CONDICIONAL DEPENDIENDO SI EXISTEN DATOS VACÍOS EN EL FORMULARIO 
+    echo '<script> alert ("Estimado Usuario, Existen Datos Vacios En El Formulario");</script>';
+    echo '<script>windows.location="registro.php"</script>';
   } else {
-      // HASH DE LA PASSWORD, SE ENCRIPTA
-      $hash_pass = password_hash($pass, PASSWORD_DEFAULT);
+    // HASH DE LA PASSWORD, SE ENCRIPTA
+    $hash_pass = password_hash($pass, PASSWORD_DEFAULT);
 
-      $consulta3 = $con->prepare("INSERT INTO usuarios (documento, nombre, apellido, correo_electronico, password, celular, direccion, id_genero, id_rol, id_estado, foto, id_tipdocu ) VALUES ('$documento','$nombre','$apellido','$email','$hash_pass', '$celular', '$direc', '$genero', '$rol', '$estado', '$foto_nombre', $tip_docu)");
-      $consulta3->execute();
+    $consulta3 = $con->prepare("INSERT INTO usuarios (documento, nombre, apellido, correo_electronico, password, celular, direccion, id_genero, id_rol, id_estado, foto, id_tipdocu ) VALUES ('$documento','$nombre','$apellido','$email','$hash_pass', '$celular', '$direc', '$genero', '$rol', '$estado', '$foto_nombre', $tip_docu)");
+    $consulta3->execute();
 
-      // Obtener el último documento de usuario insertado
-      $documento_insertado = $documento;
+    // Obtener el último documento de usuario insertado
+    $documento_insertado = $documento;
 
-      // Verificar el id_rol y actualizar el id_estado si es necesario
-      if ($rol == 3 || $rol == 6) {
-          $nuevo_estado = 2;
+    // Verificar el id_rol y actualizar el id_estado si es necesario
+    if ($rol == 3 || $rol == 6) {
+      $nuevo_estado = 2;
 
-          $consulta4 = $con->prepare("UPDATE usuarios SET id_estado = :nuevo_estado WHERE documento = :documento");
-          $consulta4->bindParam(':nuevo_estado', $nuevo_estado);
-          $consulta4->bindParam(':documento', $documento_insertado);
-          $consulta4->execute();
-      }
+      $consulta4 = $con->prepare("UPDATE usuarios SET id_estado = :nuevo_estado WHERE documento = :documento");
+      $consulta4->bindParam(':nuevo_estado', $nuevo_estado);
+      $consulta4->bindParam(':documento', $documento_insertado);
+      $consulta4->execute();
+    }
 
-      echo '<script>alert ("Registro exitoso, gracias por tu registro, ya puedes iniciar sesión.");</script>';
-      echo '<script>window.location="login.php"</script>';
+    echo '<script>alert ("Registro exitoso, gracias por tu registro, ya puedes iniciar sesión.");</script>';
+    echo '<script>window.location="login.php"</script>';
   }
 }
 
@@ -119,15 +119,13 @@ if ((isset($_POST["btn-registrar"]))) {
     <br>
     <h2>REGISTRO</h2>
 
-
-
     <form method="POST" enctype="multipart/form-data" autocomplete="off">
-    <div class="row">
+      <div class="row">
         <div class="col">
-            <label for="tipo_docu" style="margin-right: 10px;">Tipo de Documento:</label>
-            <select name="id_tipdocu" class="form-control" required id="select3">
-                <option value="" disabled selected>Seleccione Tipo de documento</option>
-                <?php
+          <label for="tipo_docu" style="margin-right: 10px;">Tipo de Documento:</label>
+          <select name="id_tipdocu" class="form-control" required id="select3">
+            <option value="" disabled selected>Seleccione Tipo de documento</option>
+            <?php
 
             do {
 
@@ -137,62 +135,17 @@ if ((isset($_POST["btn-registrar"]))) {
             } while ($consull = $consulta2->fetch());
 
             ?>
-            </select>
+          </select>
         </div>
-
         <div class="col">
-            <label for="documento" style="margin-right: 10px;">Documento:</label>
-            <input type="number" placeholder="Numero de documento" class="form-control" name="documento" id="documento" pattern="[0-9]{6,10}" onkeypress="return(multiplenumber(event));" oninput="maxlengthNumber(this);" maxlength="10" required>
+          <label for="documento" style="margin-right: 10px;">Documento:</label>
+          <input type="number" placeholder="Numero de documento" class="form-control" name="documento" id="documento" pattern="[0-9]{6,10}" onkeypress="return(multiplenumber(event));" oninput="maxlengthNumber(this);" maxlength="10" required>
         </div>
-    </div>
-
-    <div class="row">
         <div class="col">
-            <label for="nombre" style="margin-right: 10px;">Nombres:</label>
-            <input type="text" autocomplete="off" placeholder="Ingrese solo su Nombre" class="form-control" pattern="[a-z]{2,30}" onkeyup="minuscula(this)" minlength="2" maxlength="30" id="nombre" name="nombre" required title="No debe terminar con espacios; su límite es de 30 dígitos">
-        </div>
-
-        <div class="col">
-            <label for="apellido" style="margin-right: 10px;">Apellidos:</label>
-            <input type="text" autocomplete="off" placeholder="Ingrese solo sus apellidos" class="form-control" pattern="[a-z]{2,30}" onkeyup="minuscula(this)" maxlength="30" id="apellido" name="apellido" required title="No debe terminar con espacios; su límite es de 30 dígitos">
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col">
-            <label for="email" style="margin-right: 10px;">Correo Electrónico:</label>
-            <input type="email" placeholder="Correo Electronico" class="form-control" name="correo_electronico" required onkeyup="espacios(this)" maxlength="40">
-        </div>
-
-        <div class="col">
-            <label for="password">Contraseña:</label>
-            <div class="input-group">
-                <input type="password" placeholder="Contraseña" name="password" pattern="[a-zA-Z0-9]{6,12}" class="form-control input-text clave" title="Debe tener de 6 a 12 dígitos" required onkeyup="espacios(this)" minlength="6" maxlength="12" id="passwordField">
-                <div class="input-group-append">
-                    <button type="button" class="icono fas fa-eye-slash mostrarClave w-20 bg-gradient" id="togglePassword"></button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col">
-            <label for="celular" style="margin-right: 10px;">Celular:</label>
-            <input type="number" autocomplete="off" placeholder="Numero Del Celular" onkeypress="return(multiplenumber(event));" oninput="maxlengthNumber(this);" maxlength="10" class="form-control" id="celular" name="celular" required>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col">
-            <label for="direccion" style="margin-right: 10px;">Dirección:</label>
-            <input type="text" autocomplete="off" placeholder="Ingrese su dirección" class="form-control" onkeyup="validarDireccion(this)" maxlength="50" id="direccion" name="direccion" required>
-        </div>
-
-        <div class="col">
-            <label for="genero" style="margin-right: 10px;">Género:</label>
-            <select name="id_genero" class="form-control" required id="select2">
-                <option value="" disabled selected>Seleccione tipo de genero</option>
-                <?php
+          <label for="genero" style="margin-right: 10px;">Género:</label>
+          <select name="id_genero" class="form-control" required id="select2">
+            <option value="" disabled selected>Seleccione tipo de genero</option>
+            <?php
 
             do {
 
@@ -202,20 +155,51 @@ if ((isset($_POST["btn-registrar"]))) {
             } while ($consul = $consulta1->fetch());
 
             ?>
-            </select>
+          </select>
         </div>
-        <div class="form-group col">
-            <label for="imagen">Foto De Perfil:</label>
-            <input type="file" class="form-control-file" id="foto" name="foto" accept="image/*" required>
-        </div>
-    </div>
+      </div>
 
-    <div class="row">
+      <div class="row">
         <div class="col">
-            <label for="rol" style="margin-right: 10px;">Rol:</label>
-            <select name="id_rol" class="form-control" required id="select2">
-                <option value="" disabled selected>Seleccione tipo de rol</option>
-                <?php
+          <label for="nombre" style="margin-right: 10px;">Nombres:</label>
+          <input type="text" autocomplete="off" placeholder="Ingrese solo su Nombre" class="form-control" pattern="[a-z]{2,30}" onkeyup="minuscula(this)" minlength="2" maxlength="30" id="nombre" name="nombre" required title="No debe terminar con espacios; su límite es de 30 dígitos">
+        </div>
+        <div class="col">
+          <label for="apellido" style="margin-right: 10px;">Apellidos:</label>
+          <input type="text" autocomplete="off" placeholder="Ingrese solo sus apellidos" class="form-control" pattern="[a-z]{2,30}" onkeyup="minuscula(this)" maxlength="30" id="apellido" name="apellido" required title="No debe terminar con espacios; su límite es de 30 dígitos">
+        </div>
+        <div class="col">
+          <label for="email" style="margin-right: 10px;">Correo Electrónico:</label>
+          <input type="email" placeholder="Correo Electronico" class="form-control" name="correo_electronico" required onkeyup="espacios(this)" maxlength="40">
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <label for="password">Contraseña:</label>
+          <div class="input-group">
+            <input type="password" placeholder="Contraseña" name="password" pattern="[a-zA-Z0-9]{6,12}" class="form-control input-text clave" title="Debe tener de 6 a 12 dígitos" required onkeyup="espacios(this)" minlength="6" maxlength="12" id="passwordField">
+            <div class="input-group-append">
+              <button type="button" class="icono fas fa-eye-slash mostrarClave w-20 bg-gradient" id="togglePassword"></button>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <label for="celular" style="margin-right: 10px;">Celular:</label>
+          <input type="number" autocomplete="off" placeholder="Numero Del Celular" onkeypress="return(multiplenumber(event));" oninput="maxlengthNumber(this);" maxlength="10" class="form-control" id="celular" name="celular" required>
+        </div>
+        <div class="col">
+          <label for="direccion" style="margin-right: 10px;">Dirección:</label>
+          <input type="text" autocomplete="off" placeholder="Ingrese su dirección" class="form-control" onkeyup="validarDireccion(this)" maxlength="50" id="direccion" name="direccion" required>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <label for="rol" style="margin-right: 10px;">Rol:</label>
+          <select name="id_rol" class="form-control" required id="select2">
+            <option value="" disabled selected>Seleccione tipo de rol</option>
+            <?php
             do {
 
             ?>
@@ -224,20 +208,24 @@ if ((isset($_POST["btn-registrar"]))) {
             } while ($consulll = $consulta3->fetch());
 
             ?>
-            </select>
+          </select>
+        </div>
+        <div class="form-group col">
+          <label for="imagen">Foto De Perfil:</label>
+          <input type="file" class="form-control-file" id="foto" name="foto" accept="image/*" required>
         </div>
 
-        <div class="col">
-            <input type="hidden" placeholder="Estado" readonly class="form-control form-control-lg input-text " value="1" name="id_estado">
+        <div class="">
+          <input type="hidden" placeholder="Estado" readonly class="form-control form-control-lg input-text " value="1" name="id_estado">
         </div>
-    </div>
+      </div>
 
-    <div style="text-align: center;">
-    <br>
+      <div style="text-align: center;">
+        <br>
         <button type="submit" value="registrar" name="btn-registrar" class="btn_ing">REGISTRAR</button><br>
         <a class="ingresar" href="login.php" style="color: black;">Ingresar</a><br>
-    </div>
-</form>
+      </div>
+    </form>
   </div>
 </body>
 
